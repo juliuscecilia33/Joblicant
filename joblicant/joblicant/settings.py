14005e81a8e1147407eda46574d9373b159b997d
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-06-27 19:25:53 -0700

    cors installation and app creation

--- joblicant/joblicant/settings.py
+++ joblicant/joblicant/settings.py
@@ -31,15 +31,21 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'JoblicantApp.apps.JoblicantappConfig',
+    'rest_framework'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
