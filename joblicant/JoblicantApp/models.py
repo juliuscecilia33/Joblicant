61b343d9c4c3ff2dc2654efe52a0ebad2fbfec32
juliuscecilia33 <63413098+juliuscecilia33@users.noreply.github.com>
2021-06-28 16:15:32 -0700

    Update models.py

--- joblicant/JoblicantApp/models.py
+++ joblicant/JoblicantApp/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Application(models.Model):
+    JobId = models.AutoField(primary_key=True)
+    JobTitle = models.CharField(max_length=100)
+    CompanyName = models.CharField(max_length=100)
+    DateCompleted = models.DateField()
+    STATUS_OPTIONS = (
+        ('S', 'Sent'),
+        ('R', 'Rejected'),
+        ('I1', 'Interview[1]'),
+        ('I2', 'Interview[2]'),
+        ('I3', 'Interview[3]'),
+    )
+    Status = models.CharField(max_length=1, choices=STATUS_OPTIONS)
+    InterviewDate = models.DateField()
+
+class ApplicationCopy(models.Model):
+    ResumeFileName = models.CharField(max_length=100)
+    FullName = models.CharField(max_length=100)
+    Email = models.CharField(max_length=100)
+    Phone = models.IntegerField()
+    CurrentCompany = models.CharField(max_length=100)
+    LinkedIn = models.CharField(max_length=100)
+    Twitter = models.CharField(max_length=100)
+    Github = models.CharField(max_length=100)
+    Portfolio = models.CharField(max_length=100)
+    OtherWebsite = models.CharField(max_length=100)
